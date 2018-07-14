1c006ebc289bb4d4c08caa227fbd2f887e25f8fe
tianyuStar <yunxing.liu@foxmail.com>
2018-07-14 09:34:20 +0800

    创建了两个只有管理员才能看到的界面

---
+++ src/views/admin/RoleManage.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>只有管理员才能看到的权限管理</h1>
+</template>
+
+<script>
+    export default {
+        name: "RoleManage"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/admin/UserManage.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>只有管理员有权限查看此界面</h1>
+</template>
+
+<script>
+    export default {
+        name: "UserManage"
+    }
+</script>
+
+<style scoped>
+
+</style>
