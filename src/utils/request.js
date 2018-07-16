f53e1a46d80ebea0babc95141a6d058c40c3f177
lenovo <1783742430@qq.com>
2018-07-16 15:00:53 +0800

    添加设备添加地图

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://192.168.1.100:8090', // api的base_url
+  baseURL: 'http://192.168.1.125:8090', // api的base_url
   timeout: 5000 // request timeout
 })
 
